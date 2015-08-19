87bfa0d28fe3bafb2780f7a3004474e92bd4e834
Pratyaksh Sharma <pratyaksh@me.com>
2015-08-19 21:15:45 +0530

    Implemented LookupIter methods.

--- Assignment-1/LookupIter.cpp
+++ Assignment-1/LookupIter.cpp
@@ -18,26 +18,30 @@ bool LookupIter::isNull() {
 }
 
 bool LookupIter::hasNext() {
-    /*
-     * TODO: Fill in code
-     *
-     * Hint: See usage in Index.cpp.main() to understand functionality
-     * Also see LookupIter.h
-     */
+
+    int next_pos = position + 1;
+    if (next_pos >= node->numkeys) {
+    	/* Reached end of node */
+    	return false;
+    }
+    if (strncmp(&(node->data[next_pos*keylen(&keyType)]), key, keylen(&keyType)) != 0) {
+    	/* The key at next position doesn't match the iterator's key*/
+    	return false;
+    }
+    return true;
 }
 
 int LookupIter::next() {
-    /*
-     * TODO: Fill in code
-     *
-     * Hint: See usage in Index.cpp.main() to understand functionality
-     * Also see LookupIter.h
-     */
+    if (hasNext()) {
+    	position++;
+    	return 0;
+    }
+    else return -1;
 }
 
-
 int LookupIter::get(char *payload) {
-    /*
-     * TODO: Fill in code
-     */
+	/* TODO: Checks */
+	/* TODO: Advance position?*/
+    strncpy(payload, &(node->data[DATA_SIZE-(position+1)*payloadlen]), payloadlen);
+    return 0;
 }
